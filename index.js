c29edb0618a32e1efc8fd71f9471a1e4b16cf6e5
Parker Shepherd <parker.shepherd@lendio.com>
2016-06-20 12:35:23 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var elevatorMusic = require('./lib/elevator-music');
+var runCommand = require('./lib/run-command');
+
+elevatorMusic.start();
+
+runCommand(function (code) {
+  elevatorMusic.stop();
+  process.exit(code);
+})
